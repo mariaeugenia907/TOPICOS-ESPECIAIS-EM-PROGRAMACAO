d3ec9636b6957218c7cd7972d1e527d585d59305
MariaAlice00 <mariaalice021200@gmail.com>
2022-05-26 17:49:51 -0300

    consertando

--- dangerSpace/scripts/extras/Explosion.gd
+++
@@ -1,7 +0,0 @@
-extends AnimatedSprite
-
-func _ready() -> void:
-	play("Idle")
-
-func on_animation_finished() -> void:
-	queue_free()
--- dangerSpace/scripts/extras/Hit_SFX.gd
+++
@@ -1,6 +0,0 @@
-extends AudioStreamPlayer
-
-
-
-func on_sfx_finished():
-	queue_free()
